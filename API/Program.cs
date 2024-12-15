07db6c3aeb2ef929ea688478397d86bc513aa543
Gul Noureen <gul.noureen@spursol.com>
2024-12-15 15:16:50 +0500

    crud done

---
+++ API/Program.cs
@@ -0,0 +1,28 @@
+
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            var app = builder.Build();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
