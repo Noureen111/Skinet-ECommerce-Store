dd75d6aeb73a97fe27a23b1487eca6ca58152782
Gul Noureen <gul.noureen@spursol.com>
2025-01-07 00:06:15 +0500

    environments added

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cart } from '../../shared/models/cart';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  baseUrl = environment;
+  cart = signal<Cart | null>(null);
+
+  constructor(
+    private httpClient: HttpClient 
+  ) { }
+
+  getCart(id: string) {
+    return this.httpClient.get<Cart>(this.baseUrl + "cart?id=" + id).subscribe({
+      next: cart => this.cart.set(cart)
+    });
+  }
+
+  setCart(cart: Cart) {
+    return this.httpClient.post<Cart>(this.baseUrl + "cart", cart).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
 }
