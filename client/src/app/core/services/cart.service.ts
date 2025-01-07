58cca4e910f77282888b13e7e2ac58f620d21d56
Gul Noureen <gul.noureen@spursol.com>
2025-01-07 23:33:59 +0500

    cart service methods

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Cart } from '../../shared/models/cart';
+import { Cart, CartItem } from '../../shared/models/cart';
+import { Product } from '../../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -26,4 +27,49 @@ export class CartService {
       next: cart => this.cart.set(cart)
     })
   }
+
+  addItemToCart(item: CartItem | Product, quantity = 1) {
+    const cart = this.cart() ?? this.createCart();
+    if(this.isProduct(item)) {
+      item = this.mapProductToCartItem(item);
+    }
+
+    cart.items = this.addOrUpdateCart(cart.items, item, quantity);
+    this.setCart(cart);
+  }
+
+  private addOrUpdateCart(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+    const index = items.findIndex(x => x.productId === item.productId);
+    if(index === -1) {
+      item.quantity = quantity;
+      items.push(item);
+    } 
+    else {
+      items[index].quantity += quantity;
+    }
+    return items; 
+  }
+
+  private mapProductToCartItem(item: Product): CartItem {
+    return {
+      productId: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      brand: item.brand,
+      type: item.type
+    };
+  }
+
+  private isProduct(item: CartItem | Product): item is Product {
+    return (item as Product).id !== undefined;
+  } 
+
+  createCart(): Cart {
+    const cart = new Cart();
+    localStorage.setItem("cart_id", cart.id);
+    return cart;
+  }
 }
+
