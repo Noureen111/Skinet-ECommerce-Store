e0c9337d6e7467d0dc1382598046f92b4f5f3f66
Gul Noureen <gul.noureen@spursol.com>
2025-01-09 23:22:09 +0500

    cart count

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -5,6 +5,7 @@ import { MatBadgeModule } from '@angular/material/badge';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { BusyService } from '../../core/services/busy.service';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -23,6 +24,7 @@ import { BusyService } from '../../core/services/busy.service';
 export class HeaderComponent {
 
   constructor(
-    public busyService: BusyService
+    public busyService: BusyService,
+    public cartService: CartService
   ) {}
 }
