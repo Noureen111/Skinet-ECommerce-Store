5545b6d882601c3a813ec18bf413523e43282a1c
Gul Noureen <gul.noureen@spursol.com>
2025-01-30 22:47:48 +0500

    login

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account.service';
 import { Router } from '@angular/router';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -30,6 +41,7 @@ export class LoginComponent {
   }
 
   onSubmit() {
+    debugger
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
         this.accountService.getUserInfo();
