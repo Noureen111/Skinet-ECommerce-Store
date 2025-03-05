27a1c0fed1374ac6b60d11c6c86c7d6599bebca2
Gul Noureen <gul.noureen@spursol.com>
2025-03-05 06:18:35 +0500

    orders

--- Core/Entities/OrderAggregate/OrderItem.cs
+++ Core/Entities/OrderAggregate/OrderItem.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities.OrderAggregate
 {
-    public class OrderItem
+    public class OrderItem: BaseEntity
     {
         public ProductItemOrdered ItemOrdered { get; set; } = null!;
         public decimal Price { get; set; }
