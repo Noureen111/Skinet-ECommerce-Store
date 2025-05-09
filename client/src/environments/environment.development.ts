57aa0f92d666a92488576a486b17a0adec1d6248
Gul Noureen <gul.noureen@spursol.com>
2025-05-09 20:31:47 +0500

    client secret

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -4,3 +4,4 @@ export const environment = {
     hubUrl: 'https://localhost:5001/hub/notifications',
     StripePublicKey: "pk_test_51RIFVW4TZVot5K7cqucfT5Gqp4q0p1vhMZgAD1zxVjvm5x0E8iNo0YLSluKXNiLC6TIv7OyBvTUuLIrBIIlNwBUB00fPrBXr1V",
 };
+//let it be
